729cd1df77a455432a4a02ed720eabbb5a151183
SviatikKh <48651894+SviatikKh@users.noreply.github.com>
2020-10-25 14:21:49 +0200

    Added SEARCH

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -9,10 +9,18 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 # Create your views here.
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
